1498b939482672c9a95686a96b5b10c4d79bf5e8
czy <czysoft@163.com>
2022-07-18 22:41:04 +0800

    增加minode开发板的rgb灯驱动

--- czyminode.ts
+++ czyminode.ts
@@ -7,7 +7,7 @@ namespace czyminode {
 	/**
      * FanOn
      */
-    //% blockId=fan_on
+    //% blockId=打开风扇
     //% block
     export function FanOn(): void{
         const p0: DigitalInOutPin = new MicrobitPin(DigitalPin.P0);
@@ -19,7 +19,7 @@ namespace czyminode {
 	/**
      * FanOff
      */
-    //% blockId=fan_off
+    //% blockId=关闭风扇
     //% block
     export function FanOff(): void {
         const p0: DigitalInOutPin = new MicrobitPin(DigitalPin.P0);
@@ -27,5 +27,40 @@ namespace czyminode {
         const p1: DigitalInOutPin = new MicrobitPin(DigitalPin.P1);
         p1.digitalWrite(true);     
     }
+
+
+    /**
+     * RGBLED
+     */
+    //% blockId=RGB变色灯
+    //% block
+    export function RGBLed(rgb: uint32): void {
+
+        const clock: DigitalInOutPin = new MicrobitPin(DigitalPin.P12);
+        const data: DigitalInOutPin = new MicrobitPin(DigitalPin.P13);
+        clock.digitalWrite(false);
+        data.digitalWrite(false);
+
+
+        var num: number = 24;
+        var i: number;
+        var rgb2: uint32=rgb;
+
+        for (i = 0; i < num; i++) {
+            if (rgb2 & (1 << 23))
+                data.digitalWrite(true);
+            else
+                data.digitalWrite(false);
+            rgb2 <<= 1;
+
+            clock.digitalWrite(true);
+            clock.digitalWrite(false);
+        }
+
+
+    }
+
+
+
 
 }
