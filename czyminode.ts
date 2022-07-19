32d7a2aaeadac43be2add9e8c062d6014535af2c
czy <czysoft@163.com>
2022-07-19 16:38:27 +0800

    1.dht11支持 2.接线插口支持

--- czyminode.ts
+++ czyminode.ts
@@ -9,10 +9,25 @@ namespace czyminode {
      */
     //% blockId=turn_on_fan
     //% block
-    export function FanOn(): void{
-        let p0: DigitalInOutPin = new MicrobitPin(DigitalPin.P0);
+    export function FanOn(ConnName: ConnNameA): void{
+        let p0: DigitalInOutPin;
+        let p1: DigitalInOutPin;
+
+        if (ConnName == ConnNameA.A0)
+        {
+            p0= new MicrobitPin(DigitalPin.P0);
+            p1 = new MicrobitPin(DigitalPin.P1);
+        }
+        else if (ConnName == ConnNameA.A1) {
+            p0 = new MicrobitPin(DigitalPin.P1);
+            p1 = new MicrobitPin(DigitalPin.P2);
+        }
+        else if (ConnName == ConnNameA.A2) {
+            p0 = new MicrobitPin(DigitalPin.P2);
+            p1 = new MicrobitPin(DigitalPin.P3);
+        }
+
         p0.digitalWrite(true);
-        let p1: DigitalInOutPin = new MicrobitPin(DigitalPin.P1);
         p1.digitalWrite(false);        
     }
 
@@ -21,14 +36,34 @@ namespace czyminode {
      */
     //% blockId=turn_off_fan
     //% block
-    export function FanOff(): void {
-        let p0: DigitalInOutPin = new MicrobitPin(DigitalPin.P0);
+    export function FanOff(ConnName: ConnNameA): void {
+        let p0: DigitalInOutPin;
+        let p1: DigitalInOutPin;
+
+        if (ConnName == ConnNameA.A0) {
+            p0 = new MicrobitPin(DigitalPin.P0);
+            p1 = new MicrobitPin(DigitalPin.P1);
+        }
+        else if (ConnName == ConnNameA.A1) {
+            p0 = new MicrobitPin(DigitalPin.P1);
+            p1 = new MicrobitPin(DigitalPin.P2);
+        }
+        else if (ConnName == ConnNameA.A2) {
+            p0 = new MicrobitPin(DigitalPin.P2);
+            p1 = new MicrobitPin(DigitalPin.P3);
+        }
+
         p0.digitalWrite(false);
-        let p1: DigitalInOutPin = new MicrobitPin(DigitalPin.P1);
         p1.digitalWrite(true);     
     }
     function delay(): void {
         basic.pause(1);
+    }
+    function delayUs(us: number): void {
+        let i;
+        for (i = 0; i < us; i++) {
+
+        }
     }
 
     /**
@@ -36,19 +71,33 @@ namespace czyminode {
      */
     //% blockId=RGB_light
     //% block
-    export function RgbLed(rgb: number): void {
+    export function RgbLed(ConnName: ConnNameD, rgb: number): void {
+        let clock: DigitalInOutPin;
+        let data: DigitalInOutPin;
 
         let num: number = 24;
         let i: number = 0;
         let rgb2: number = rgb;
-        let clock: DigitalInOutPin = new MicrobitPin(DigitalPin.P12);
-        let data: DigitalInOutPin = new MicrobitPin(DigitalPin.P13);
-        //console.log("RgbLed start");
+
+        if (ConnName == ConnNameD.D12) {
+            clock = new MicrobitPin(DigitalPin.P12);
+            data = new MicrobitPin(DigitalPin.P13);
+        }
+        else if (ConnName == ConnNameD.D13) {
+            clock = new MicrobitPin(DigitalPin.P13);
+            data = new MicrobitPin(DigitalPin.P14);
+        }
+        else if (ConnName == ConnNameD.D14) {
+            clock = new MicrobitPin(DigitalPin.P14);
+            data = new MicrobitPin(DigitalPin.P15);
+        }
+        else if (ConnName == ConnNameD.D15) {
+            clock = new MicrobitPin(DigitalPin.P15);
+            data = new MicrobitPin(DigitalPin.P16);
+        }
+
         clock.digitalWrite(false);
-
         data.digitalWrite(false);
-
-        //console.log("RgbLed inited");
 
 
         for (i = 0; i < 32; i++) {
@@ -71,13 +120,261 @@ namespace czyminode {
             delay();
             clock.digitalWrite(false);
             delay();
-            //console.log("RgbLed loop "+i.toString());
         }
 
 
     }
 
 
+    /**
+     * SpeakerOut
+     */
+    //% blockId=Speaker_Out
+    //% block
+    export function SpeakerOut(ConnName: ConnNameD, vol: number): void {
+        let p1: AnalogOutPin = new MicrobitPin(DigitalPin.P1);
+        let p2: AnalogOutPin = new MicrobitPin(DigitalPin.P2);
 
+        p1.analogWrite(vol);
+        p2.analogWrite(vol);
+    }
+    
+
+    /**
+     * Get DHT11 temperature (celsius or fahrenheit).
+     */
+    //% blockId=minode_dht_get_temperature block="dht11 %connName| tempreature %FanStatus" 
+    export function DHTGetTemperature(connName: ConnNameD, style: DHTTemStyle): number {
+        let p0: DigitalInOutPin;
+        let p1: DigitalInOutPin;
+        let i: number;
+        let data: number;
+        let temp: number;
+        let temp2: number;
+        let humi: number;
+        //let str: string;
+        //let bitarr: number[] = [0,0,0,0,0,0,0,0];
+        if (connName == ConnNameD.D12) {
+            p0 = new MicrobitPin(DigitalPin.P12);
+            p1 = new MicrobitPin(DigitalPin.P13);
+        }
+        else if (connName == ConnNameD.D13) {
+            p0 = new MicrobitPin(DigitalPin.P13);
+            p1 = new MicrobitPin(DigitalPin.P14);
+        }
+        else if (connName == ConnNameD.D14) {
+            p0 = new MicrobitPin(DigitalPin.P14);
+            p1 = new MicrobitPin(DigitalPin.P15);
+        }
+        else if (connName == ConnNameD.D15) {
+            p0 = new MicrobitPin(DigitalPin.P15);
+            p1 = new MicrobitPin(DigitalPin.P16);
+        }
+        //主机开始
+        //console.log("host start");
+        p0.digitalWrite(false);
+        basic.pause(18);
+        p0.digitalWrite(true);
+        delayUs(30);
+
+        //console.log("wait dht");
+        //dht响应
+        while (p0.digitalRead()) {
+            delayUs(1);
+        }
+        //console.log("wait dht 1");
+        let time: number = 0;
+        while (p0.digitalRead()==false) {       //低电平计数
+            time++;
+        }
+        //console.log("wait dht 2");
+        time = 0;
+        while (p0.digitalRead() == true) {       //高电平计数
+            time++;
+        }
+        //开始传数据
+
+        //console.log("start get data");
+
+        time = 0;
+        humi = 0;
+        for (i = 0; i < 8; i++) {
+            while (p0.digitalRead() == false) {       //低电平计数
+                time++;
+            }
+            time = 0;
+            while (p0.digitalRead() == true) {       //高电平计数
+                time++;
+            }
+            //bitarr[i] = time;
+            //console.log("bit time:" + time.toString());
+            humi = humi << 1;
+            if (time > 7)
+                humi |= 1;
+        }
+
+
+        //str = "";
+        //for (i = 0; i < 8;i++) {
+        //    str += bitarr[i].toString()+ ",";
+        //}
+        //console.log("bit:" + str);
+        //time = 0;
+        //humi = 0;
+        for (i = 0; i < 8; i++) {
+            while (p0.digitalRead() == false) {       //低电平计数
+                time++;
+            }
+            time = 0;
+            while (p0.digitalRead() == true) {       //高电平计数
+                time++;
+            }
+            //bitarr[i] = time;
+            //console.log("bit time:" + time.toString());
+           // humi = humi << 1;
+            //if (time > 7)
+           //     humi |= 1;
+        }
+        time = 0;
+        temp = 0;
+        for (i = 0; i < 8; i++) {
+            while (p0.digitalRead() == false) {       //低电平计数
+                time++;
+            }
+            time = 0;
+            while (p0.digitalRead() == true) {       //高电平计数
+                time++;
+            }
+            //bitarr[i] = time;
+            //console.log("bit time:" + time.toString());
+            temp = temp << 1;
+            if (time > 7)
+                temp |= 1;
+        }
+        time = 0;
+        temp2 = 0;
+        for (i = 0; i < 8; i++) {
+            while (p0.digitalRead() == false) {       //低电平计数
+                time++;
+            }
+            time = 0;
+            while (p0.digitalRead() == true) {       //高电平计数
+                time++;
+            }
+            //bitarr[i] = time;
+            //console.log("bit time:" + time.toString());
+            temp2 = temp2 << 1;
+            if (time > 7)
+                temp2 |= 1;
+        }
+
+        temp = parseFloat(temp.toString() + "." + temp2.toString());
+       // console.log("humi:" + humi.toString());
+        //console.log("temp:" + temp.toString());
+        return temp;
+    }
+
+    /**
+     * Get DHT11 Humidity.
+     */
+    //% blockId=minode_dht_get_humidity block="dht11 %connName| humidity"
+    //% advanced=true
+    export function DHTGetHumidity(connName: ConnNameD): number {
+        let p0: DigitalInOutPin;
+        let p1: DigitalInOutPin;
+        let i: number;
+        let data: number;
+        let humi: number;
+        if (connName == ConnNameD.D12) {
+            p0 = new MicrobitPin(DigitalPin.P12);
+            p1 = new MicrobitPin(DigitalPin.P13);
+        }
+        else if (connName == ConnNameD.D13) {
+            p0 = new MicrobitPin(DigitalPin.P13);
+            p1 = new MicrobitPin(DigitalPin.P14);
+        }
+        else if (connName == ConnNameD.D14) {
+            p0 = new MicrobitPin(DigitalPin.P14);
+            p1 = new MicrobitPin(DigitalPin.P15);
+        }
+        else if (connName == ConnNameD.D15) {
+            p0 = new MicrobitPin(DigitalPin.P15);
+            p1 = new MicrobitPin(DigitalPin.P16);
+        }
+        //主机开始
+        //console.log("host start");
+        p0.digitalWrite(false);
+        basic.pause(18);
+        p0.digitalWrite(true);
+        delayUs(30);
+
+        //console.log("wait dht");
+        //dht响应
+        while (p0.digitalRead()) {
+            delayUs(1);
+        }
+        //console.log("wait dht 1");
+        let time: number = 0;
+        while (p0.digitalRead() == false) {       //低电平计数
+            time++;
+        }
+        //console.log("wait dht 2");
+        time = 0;
+        while (p0.digitalRead() == true) {       //高电平计数
+            time++;
+        }
+        //开始传数据
+
+        //console.log("start get data");
+
+        time = 0;
+        humi = 0;
+        for (i = 0; i < 8; i++) {
+            while (p0.digitalRead() == false) {       //低电平计数
+                time++;
+            }
+            time = 0;
+            while (p0.digitalRead() == true) {       //高电平计数
+                time++;
+            }
+            //console.log("bit time:" + time.toString());
+            humi = humi << 1;
+            if (time > 7)
+                humi |= 1;
+        }
+
+
+        return humi;
+    }
+
+    /**
+     * Do something when DHT11 temperature change.
+     */
+    //% blockId=minode_on_dhttemperature_change block="dht11 %connName| on temperature change"
+    //% advanced=true
+    //% shim=minode::onDHTEvent
+    export function onDHTEvent(connName: ConnNames, body: () => void): void {
+        return;
+    }
+
+    /**
+     * Do something when a switch is opened/closed
+     * @param switchId a switch ID .
+     * @param connName MiNode Connector Name
+     * @param event Event to listen
+     */
+    //% blockId=minode_on_switch_event block="switch %connName| on %event"
+    export function onSwitchEvent(connName: ConnNames, event: SwitchEvent, body: () => void): void {
+        return;
+    }
+
+    /**
+     * Get the switch state (open or not).
+     */
+    //% blockId=minode_switch_is_opened block="switch %connName| is opened"
+    //% advanced=true
+    export function switchIsOpened(connName: ConnNames): boolean {
+        return true;
+    }
 
 }
--- enums.d.ts
+++ enums.d.ts
@@ -1,5 +1,58 @@
 // Auto-generated. Do not edit.
 declare namespace czyminode {
+    const enum SwitchEvent {
+        //% block="open" enumval=1
+        MINODE_SWITCH_EVT_OPEN = 1,
+        //% block="close" enumval=2
+        MINODE_SWITCH_EVT_CLOSE = 2,
+    }
+    const enum DHTTemStyle {
+        //% block="Celsius" enumval=1
+        MINODE_DHT_CELSIUS = 1,
+        //% block="Fahrenheit" enumval=2
+        MINODE_FAN_FAHRENHEIT = 2,
+    }
+
+    const enum ConnNames {
+        //% block="A0" enumval=1
+        A0 = 1,
+        //% block="A1" enumval=2
+        A1 = 2,
+        //% block="A2" enumval=3
+        A2 = 3,
+        //% block="D12" enumval=4
+        D12 = 4,
+        //% block="D13" enumval=5
+        D13 = 5,
+        //% block="D14" enumval=6
+        D14 = 6,
+        //% block="D15" enumval=7
+        D15 = 7,
+        //% block="I2C7" enumval=8
+        I2C1 = 8,
+        //% block="I2C2" enumval=9
+        I2C2 = 9,
+        //% block="I2C3" enumval=10
+        I2C3 = 10
+    }
+    const enum ConnNameA {
+        //% block="A0" enumval=1
+        A0 = 1,
+        //% block="A1" enumval=2
+        A1 = 2,
+        //% block="A2" enumval=3
+        A2 = 3
+    }
+    const enum ConnNameD {
+        //% block="D12" enumval=4
+        D12 = 4,
+        //% block="D13" enumval=5
+        D13 = 5,
+        //% block="D14" enumval=6
+        D14 = 6,
+        //% block="D15" enumval=7
+        D15 = 7
+    }
 }
 
 // Auto-generated. Do not edit. Really.
